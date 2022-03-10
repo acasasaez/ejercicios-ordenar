57c3f03b92faa6a3bdcd488a4c7e298ea88c3ab3
acasasaez <acasasae@myuax.com>
2022-03-10 17:22:50 +0100

    mains

---
+++ Ejercicios.py/main.py
@@ -0,0 +1,3 @@
+import prueba
+print("hola")
+prueba.hola ()
--- Ejercicios.py/prueba.py
+++ Ejercicios.py/prueba.py
@@ -1,5 +1,11 @@
-lista=[1,2,3,4,5]
-segmento = lista[2:5]
-segmento[0] = 7
-lista[2:5] = segmento
-print (lista)
+from unicodedata import name
+
+def hola():
+    lista=[1,2,3,4,5]
+    segmento = lista[2:5]
+    segmento[0] = 7
+    lista[2:5] = segmento
+    print (lista)
+
+if __name__ == "__main__":
+    hola()
