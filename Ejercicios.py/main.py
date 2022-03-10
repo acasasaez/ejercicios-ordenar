69f2d9f3bb7d2bf7ed4e73e544d261a77bcf1fec
acasasaez <acasasae@myuax.com>
2022-03-10 17:24:57 +0100

    comit

--- Ejercicios.py/main.py
+++ Ejercicios.py/main.py
@@ -1,3 +1,7 @@
 import prueba
 print("hola")
 prueba.hola ()
+#import ordenacon_por_dicotomia
+#import ordenar_por_topologia
+#lo dejo a modo de comentario porque como ya expliqué simplemente lo he puesto por cuestión imbólica, pues al no haber código no se ejecutará nada por eso lo escribo como comentario
+import intento
