d6c88b739614d9f7435520a5402d8e9c731ca450
acasasaez <acasasae@myuax.com>
2022-03-13 17:29:31 +0100

    Update main.py

--- Ejercicios.py/main.py
+++ Ejercicios.py/main.py
@@ -1,9 +1,13 @@
 import prueba
 import ordenacon_por_dicotomia
 import intento
-
+#import ordenar_por_topologia
 prueba.hola ()
 
 print(ordenacon_por_dicotomia.ordenar_dicotomia([2,8,4,7,3,1]))
 
 print (intento.intercambio_segmento([18,20,12,1,3,4,6,7,8],2,7))
+
+#print(ordenar_por_topologia.ordenar_topologia(x,z,v,w)) #Como no he escrito código lo he dejado como comentario
+#para que se muestre como sería la forma de entregarlo, pero evitando que al intentar ejecutar las funciones que están a código
+#de error alllegar a esta línea
